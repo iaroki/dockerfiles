681ea619f8ff89f0b6ff2fb8b39da2016e29512d
iaroki <iaroki@protonmail.com>
2017-12-17 09:48:23 +0200

    shebang

--- voidlinux/bootstrap.sh
+++ voidlinux/bootstrap.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 mkdir docker-void
 xbps-install -y -r $PWD/docker-void --repository=http://repo.voidlinux.eu/current -S base-voidstrap
 tar -pC docker-void -c . | docker import - <REPOSITORY NAME>
