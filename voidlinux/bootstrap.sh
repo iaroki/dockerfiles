d5d4d1796cd1fdba5c5e320e616815e3c1dd608a
iaroki <iaroki@protonmail.com>
2017-12-21 18:10:28 +0200

    delete chache

--- voidlinux/bootstrap.sh
+++ voidlinux/bootstrap.sh
@@ -3,6 +3,7 @@
 IMAGE="voidlinux"
 sudo mkdir ${IMAGE}
 sudo xbps-install -y -r ${PWD}/${IMAGE} --repository=http://repo.voidlinux.eu/current -S base-voidstrap
+sudo rm -rf ${PWD}/${IMAGE}/usr/share/doc ${PWD}/${IMAGE}/usr/share/man ${PWD}/${IMAGE}/var/cache/xbps
 sudo tar -pC ${IMAGE} -c . | docker import - iaroki/${IMAGE}
 sudo rm -rf ${IMAGE}
 
