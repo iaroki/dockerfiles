0fc026ca706de2147b221bb75f99295467345056
iaroki <iaroki@protonmail.com>
2017-12-25 18:41:04 +0200

    slstatus make

--- dwmbuild/build.sh
+++ dwmbuild/build.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-BINDIR="/src/bin"
+SRC_DIR="/src"
+BIN_DIR="${SRC_DIR}/bin"
+ST_DIR="${SRC_DIR}/st"
+SL_DIR="${SRC_DIR}/slstatus"
 
-if [ ! -d ${BINDIR} ]; then
-    mkdir ${BINDIR}
+if [ ! -d ${BIN_DIR} ]; then
+    mkdir ${BIN_DIR}
 fi
 
-cd /src
+cd ${SRC_DIR}
 make
-mv -v dwm ${BINDIR}
+mv -v dwm ${BIN_DIR}
 rm -vf config.h drw.o dwm.o util.o
-cd st
+cd ${ST_DIR}
 make
-mv -v st ${BINDIR}
+mv -v st ${BIN_DIR}
 rm -vf config.h st.o
-chown -vR 1000:1000 ${BINDIR}
+cd ${SL_DIR}
+make
+mv -v slstatus ${BIN_DIR}
+rm -vf config.h slstatus.o util.o
+chown -vR 1000:1000 ${BIN_DIR}
 echo "DONE!"
