71a49d0d9d0ea90178e40c3e4c52e4189d4b7258
iaroki <iaroki@protonmail.com>
2017-12-25 18:49:45 +0200

    sl build fix

--- dwmbuild/build.sh
+++ dwmbuild/build.sh
@@ -20,6 +20,25 @@ rm -vf config.h st.o
 cd ${SL_DIR}
 make
 mv -v slstatus ${BIN_DIR}
-rm -vf config.h slstatus.o util.o
+rm -vf config.h slstatus.o util.o \
+        components/battery.o \
+        components/cpu.o \
+        components/datetime.o \
+        components/disk.o \
+        components/entropy.o \
+        components/hostname.o \
+        components/ip.o \
+        components/kernel_release.o \
+        components/keyboard_indicators.o \
+        components/load_avg.o \
+        components/num_files.o \
+        components/ram.o \
+        components/run_command.o \
+        components/swap.o \
+        components/temperature.o \
+        components/uptime.o \
+        components/user.o \
+        components/volume.o \
+        components/wifi.o
 chown -vR 1000:1000 ${BIN_DIR}
 echo "DONE!"
