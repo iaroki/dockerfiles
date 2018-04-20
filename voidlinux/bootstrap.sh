da0f726685e7a495d9399598cea353d75c65a335
iaroki <iaroki@protonmail.com>
2018-04-20 15:21:05 +0300

    add detail output

--- voidlinux/bootstrap.sh
+++ voidlinux/bootstrap.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 IMAGE="voidlinux"
-sudo mkdir ${IMAGE}
+echo "==> Creating void directory..."
+sudo mkdir -v ${IMAGE}
+echo "==> Bootctrapping base system..."
 sudo xbps-install -y -r ${PWD}/${IMAGE} --repository=http://repo.voidlinux.eu/current -S base-voidstrap
+echo "==> Cleaning cache..."
 sudo rm -rf ${PWD}/${IMAGE}/usr/share/doc ${PWD}/${IMAGE}/usr/share/man ${PWD}/${IMAGE}/var/cache/xbps
+echo "==> Compressing and importing image..."
 sudo tar -pC ${IMAGE} -c . | docker import - iaroki/${IMAGE}
+echo "==> Removing temporary files..."
 sudo rm -rf ${IMAGE}
+echo "DONE!"
 
