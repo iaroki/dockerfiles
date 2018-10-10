fb89d79c97b5d78b8c61ac4cf4827ed941d3c206
iaroki <iaroki@protonmail.com>
2018-10-10 12:29:55 +0300

    typo

--- voidlinux/bootstrap.sh
+++ voidlinux/bootstrap.sh
@@ -3,7 +3,7 @@
 IMAGE="voidlinux"
 echo "==> Creating void directory..."
 sudo mkdir -v ${IMAGE}
-echo "==> Bootctrapping base system..."
+echo "==> Bootstrapping base system..."
 sudo xbps-install -y -r ${PWD}/${IMAGE} --repository=http://repo.voidlinux.eu/current -S base-voidstrap
 echo "==> Cleaning cache..."
 sudo rm -rf ${PWD}/${IMAGE}/usr/share/doc ${PWD}/${IMAGE}/usr/share/man ${PWD}/${IMAGE}/var/cache/xbps
