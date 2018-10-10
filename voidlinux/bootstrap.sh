16b6c69c5fc0c95488bb57924d92ec9c9087a475
iaroki <iaroki@protonmail.com>
2018-10-10 12:53:58 +0300

    new repo

--- voidlinux/bootstrap.sh
+++ voidlinux/bootstrap.sh
@@ -4,7 +4,7 @@ IMAGE="voidlinux"
 echo "==> Creating void directory..."
 sudo mkdir -v ${IMAGE}
 echo "==> Bootstrapping base system..."
-sudo xbps-install -y -r ${PWD}/${IMAGE} --repository=http://repo.voidlinux.eu/current -S base-voidstrap
+sudo xbps-install -y -r ${PWD}/${IMAGE} --repository=https://alpha.de.repo.voidlinux.org -S base-voidstrap
 echo "==> Cleaning cache..."
 sudo rm -rf ${PWD}/${IMAGE}/usr/share/doc ${PWD}/${IMAGE}/usr/share/man ${PWD}/${IMAGE}/var/cache/xbps
 echo "==> Compressing and importing image..."
